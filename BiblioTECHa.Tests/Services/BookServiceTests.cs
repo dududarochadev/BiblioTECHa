d2d234920469278f4437cfa605aa645a5e8a695c
Dudu da Rocha <eduardoldarocha@gmail.com>
2023-12-01 14:45:07 -0300

    test: tests implementeds

--- BiblioTECHa.Tests/Services/BookServiceTests.cs
+++ BiblioTECHa.Tests/Services/BookServiceTests.cs
@@ -1,22 +1,39 @@
+using BiblioTECHa.Domain.Dtos;
+using BiblioTECHa.Domain.Models;
 using BiblioTECHa.Repositories.Interfaces;
 using BiblioTECHa.Services;
+using Microsoft.AspNetCore.JsonPatch;
 using Moq;
 
 namespace BiblioTECHa.Tests.Services;
 
 public class BookServiceTests
 {
-    private BookService _bookService;
+    private BookService _service;
 
     public BookServiceTests()
     {
-        _bookService = new BookService(new Mock<IBookRepository>().Object);
+        _service = new BookService(new Mock<IBookRepository>().Object);
     }
 
     [Fact]
-    public void GetById_SendingInvalidId()
+    public void GetById_InvalidId()
     {
-        var exception = Assert.Throws<Exception>(() => _bookService.GetById(0));
+        var exception = Assert.Throws<Exception>(() => _service.GetById(0));
+        Assert.Equal("ID inválido.", exception.Message);
+    }
+
+    [Fact]
+    public void Update_InvalidId()
+    {
+        var exception = Assert.Throws<Exception>(() => _service.Update(0, new JsonPatchDocument<BookDto>())); ;
+        Assert.Equal("ID inválido.", exception.Message);
+    }
+
+    [Fact]
+    public void Delete_InvalidId()
+    {
+        var exception = Assert.Throws<Exception>(() => _service.Delete(0)); ;
         Assert.Equal("ID inválido.", exception.Message);
     }
 }
